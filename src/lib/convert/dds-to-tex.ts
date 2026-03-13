eddf6a36ae3d0e98f2aa5148085f628722c63f80
Skia <nedraenintyel@gmail.com>
2026-03-13 10:58:59 -0600

    Build binary tex file in a very ugly way

--- src/lib/convert/dds-to-tex.ts
+++ src/lib/convert/dds-to-tex.ts
@@ -195,11 +195,53 @@ export async function ddsToTex(file: File) {
     height: header.height,
     depth: header.depth,
     mipLevels: Math.min(header.mipMapCount, 13),
+    arraySize: 0, // 0 if 2D texture
     lodIndices: [0, 1, 2],
     offsetsToSurfaces: toTexOffsetArray(ddsInfo),
   };
 
+  const headerBuffer = new ArrayBuffer(
+    4 + // attributes
+    4 + // format
+    2 + // width
+    2 + // height
+    2 + // depth
+    1 + // mipLevels
+    1 + // arraySize
+    (4 * 3) + // lod indices
+    (4 * 13) // offsets to surfaces
+  );
+  const headerBufferView = new DataView(headerBuffer);
+  headerBufferView.setUint32(0, texHeader.attributes, true);
+  headerBufferView.setUint32(4, texHeader.format, true);
+  headerBufferView.setUint16(8, texHeader.width, true);
+  headerBufferView.setUint16(10, texHeader.height, true);
+  headerBufferView.setUint16(12, texHeader.depth, true);
+  headerBufferView.setUint16(14, texHeader.mipLevels, true);
+  headerBufferView.setUint16(15, texHeader.arraySize, true);
+  texHeader.lodIndices.forEach((v, i) => {
+    headerBufferView.setUint32(16 + (i * 4), v, true);
+  });
+  texHeader.offsetsToSurfaces.forEach((v, i) => {
+    headerBufferView.setUint32(28 + (i * 4), v, true);
+  });
+
+  const bodyBuffer = body._raw.buffer;
+
+  const headerBufferArray = new Uint8Array(headerBuffer);
+  const bodyBufferArray = body._raw;
+
+  const fileBuffer = new ArrayBuffer(headerBuffer.byteLength + bodyBuffer.byteLength);
+  const fileBufferView = new DataView(fileBuffer);
+  headerBufferArray.forEach((v, i) => {
+    fileBufferView.setUint8(i, v);
+  });
+  bodyBufferArray.forEach((v, i) => {
+    fileBufferView.setUint8(headerBuffer.byteLength + i, v);
+  });
+
   return {
     header: texHeader,
+    buffer: fileBuffer,
   };
 }
